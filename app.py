307eff97aa43bd57cf67aa458505153fa33107d6
Jacob Heinrich <heinrija@oregonstate.edu>
2022-04-04 15:19:42 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+apps = []
+
+if os.path.isfile('save.txt'):
+    with open('save.txt','r') as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(',')
+        apps = [x for x in tempApps if x.strip()]
+
+
+def addApp():
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    filename = filedialog.askopenfilename(initialdir='/',title='Select File',
+                                         filetypes=(('executables', '*.app'),
+                                                    ('all files','*.*')))
+    apps.append(filename)
+
+    for app in apps:
+        label = tk.Label(frame,text=app,bg="gray")
+        label.pack()
+
+def runApps():
+    for app in apps:
+        os.system("open {}".format(app))
+
+def deleteApp():
+    user_input = input()
+    for widget in frame.winfo_children():
+        if user_input == widget.cget('text'):
+            widget.destroy()
+    apps.remove(user_input)
+    with open('save.txt', 'w') as f:
+        for app in apps:
+            f.write(app + ',')
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10,
+                     pady=5, fg="black",command=addApp)
+openFile.pack()
+runApps = tk.Button(root, text="Run Files", padx=10,
+                    pady=5, fg="black",command=runApps)
+runApps.pack()
+
+deleteFile = tk.Button(root,text="Delete File", padx= 10,
+                       pady=5, fg='black',command=deleteApp)
+deleteFile.pack()
+
+
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
+root.mainloop()
+
+
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
